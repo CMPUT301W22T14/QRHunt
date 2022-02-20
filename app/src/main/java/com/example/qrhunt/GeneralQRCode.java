a5bf6b190aee513501185e0fe4d16a59a47e678e
xiaofeiyu2019 <yufei24@ualberta.ca>
2022-02-20 13:53:39 -0700

    add GeneralQRCode

---
+++ app/src/main/java/com/example/qrhunt/GeneralQRCode.java
@@ -0,0 +1,13 @@
+package com.example.qrhunt;
+
+public abstract class GeneralQRCode {
+    protected String content;
+
+    public GeneralQRCode(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
